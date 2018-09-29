d3c77427dd3122e675487cfbe1eb37a40af646f0
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-09-29 19:45:37 -0300

    stylise titles

--- reanimar/src/components/TitleScreen.vue
+++ reanimar/src/components/TitleScreen.vue
@@ -13,7 +13,14 @@ export default {
 <style lang='sass' scoped>
 @import ../transform-center-mixin
 
+.screen
+  background-color: black
+  text-shadow: 0 0 10px #ededed20
+
 h2
   +transformCenter
+  text-transform: uppercase
+  font-weight: bold
+  color: #dfdfdf
 
 </style>
