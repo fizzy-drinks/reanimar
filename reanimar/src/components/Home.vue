a014aac934b9a34bf6ed4dacb25d2d48789c1986
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-09-29 15:45:13 -0300

    create title screens

--- reanimar/src/components/Home.vue
+++ reanimar/src/components/Home.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
 .home
   video-screen
+  title-screen(:title='actOneTitle')
+  title-screen(:title='actTwoTitle')
+  title-screen(:title='actThreeTitle')
 
 </template>
 
 <script>
 import VideoScreen from './VideoScreen'
+import TitleScreen from './TitleScreen'
 import PageScroller from './PageScroller'
 
 export default {
   name: 'Home',
-  components: { VideoScreen },
+  components: { VideoScreen, TitleScreen },
   mixins: [PageScroller],
+  created () {
+    this.actOneTitle = 'Ato I: O Problema'
+    this.actTwoTitle = 'Ato II: As Causas'
+    this.actThreeTitle = 'Ato III: A Solução'
+  },
   mounted () {
     window.addEventListener('keydown', event => {
       if (!event.key.match(/arrow/i)) return
---
+++ reanimar/src/components/TitleScreen.vue
@@ -0,0 +1,11 @@
+<template lang='pug'>
+section.screen
+  h2 {{ title }}
+</template>
+
+<script>
+export default {
+  name: 'TitleScreen',
+  props: ['title']
+}
+</script>
