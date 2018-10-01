3f5f1123cc86052f20f2cd861d2724e68ed9a29e
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-01 20:51:48 -0300

    fix path resolution

--- reanimar/config/index.js
+++ reanimar/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../..'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/reanimar',
+    assetsPublicPath: '/reanimar/',
 
     /**
      * Source Maps
