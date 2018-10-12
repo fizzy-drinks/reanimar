058fbdf5f73631404315cfe46b35b2fa9cd81f1b
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-12 19:04:32 -0300

    add video support

--- reanimar/src/components/TextScreen.vue
+++ reanimar/src/components/TextScreen.vue
@@ -3,15 +3,24 @@ section.screen
   .centered
     h2 {{ title }}
     div(v-for='(section, i) in content' :key='i')
-      audio(v-if='section.audio' :src='section.audio' controls)
+      img(
+        v-if='section.picture'
+        :alt='section.picture.alt'
+        :src='section.picture.src'
+      )
       p(v-if='section.text' v-html='md(section.text)')
+      audio(v-if='section.audio' :src='section.audio' controls)
+      youtube-video(v-if='section.video' :id='section.video')
 </template>
 
 <script>
 import { markdown } from 'markdown'
 
+import YoutubeVideo from './YoutubeVideo'
+
 export default {
   name: 'TextScreen',
+  components: { YoutubeVideo },
   props: ['title', 'content'],
   methods: {
     md (text) {
---
+++ reanimar/src/components/YoutubeVideo.vue
@@ -0,0 +1,21 @@
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="youtubeUrl"
+    frameborder="0"
+    allow="autoplay; encrypted-media"
+    allowfullscreen></iframe>
+</template>
+
+<script>
+export default {
+  name: 'YoutubeVideo',
+  props: ['id'],
+  computed: {
+    youtubeUrl () {
+      return `https://www.youtube.com/embed/${this.id}`
+    }
+  }
+}
+</script>
