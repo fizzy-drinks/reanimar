b4e850ba27ff5dafad207fa1222665b9024cc259
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-15 21:24:51 -0300

    import router components asynchronously

--- reanimar/src/router/index.js
+++ reanimar/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import ActOne from '@/components/ActOne'
+
+const Home = () => import('@/components/Home')
+const ActOne = () => import('@/components/ActOne')
 
 Vue.use(Router)
 
