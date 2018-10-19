3bc5e24d3cb957d7912b20cea11b86fa14417d0e
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-18 22:06:56 -0300

    add transition animation

--- reanimar/src/App.vue
+++ reanimar/src/App.vue
@@ -1,9 +1,28 @@
 <template lang='pug'>
 #app
   router-view
+  .loader(:class='{ active: loading }')
+    | Carregando...
+    .pong
 
 </template>
 
 <script>
-export default { name: 'MainApp' }
+export default {
+  name: 'MainApp',
+  data () {
+    return {
+      loading: false
+    }
+  },
+  mounted () {
+    this.$router.beforeEach((to, from, next) => {
+      this.loading = true
+      window.setTimeout(next, 400)
+    })
+    this.$router.afterEach((to, from) => {
+      window.setTimeout(() => { this.loading = false }, 400)
+    })
+  }
+}
 </script>
