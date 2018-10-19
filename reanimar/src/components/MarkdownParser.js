79b930c437e67de31a23ad95f91bc827d8aca245
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-18 22:20:11 -0300

    upgrade markdown parser

--- reanimar/src/components/MarkdownParser.js
+++ reanimar/src/components/MarkdownParser.js
@@ -1,9 +1,13 @@
-import { markdown } from 'markdown'
+import Markdown from 'markdown-it'
 
 export default {
   methods: {
     md (text) {
-      return markdown.toHTML(text)
+      return Markdown({
+        typographer: true,
+        html: true,
+        quotes: '“”‘’'
+      }).render(text)
     }
   }
 }
