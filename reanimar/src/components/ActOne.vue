381b07c4d6cf63c6cedb1660ad03d6070ad224b8
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-18 22:35:29 -0300

    remove command palette

--- reanimar/src/components/ActOne.vue
+++ reanimar/src/components/ActOne.vue
@@ -7,61 +7,18 @@
     ref='page'
   )
 
-  div(v-if='command' style='position: fixed; top: 5vh; left: 0; width: 100%')
-    input(style='display: block; margin: 0 auto; width: 15em' v-model='command')
-
   back-button
 </template>
 
 <script>
 import TextManager from './TextManager'
+import TitleScreen from './TitleScreen'
 import TextScreen from './TextScreen'
 import BackButton from './BackButton'
 
 export default {
   name: 'ActOne',
   mixins: [TextManager],
-  components: { TextScreen, BackButton },
-  data () {
-    return {
-      currentPage: 0,
-      command: ''
-    }
-  },
-  methods: {
-    goToPage (num) {
-      window.scrollTo({
-        top: this.$refs.page[num].$el.offsetTop,
-        left: 0,
-        behavior: 'smooth'
-      })
-    },
-    processCommand (cmd) {
-      if (cmd === 'prox' && this.currentPage < this.$refs.page.length - 1) {
-        this.currentPage += 1
-      }
-      if (cmd === 'ant' && this.currentPage) this.currentPage -= 1
-    },
-    cmdListener (event) {
-      if (event.key === 'Enter') {
-        const currentCommand = this.command
-        this.processCommand(currentCommand)
-        this.command = ''
-      }
-      if (!event.key.match(/^\w$/)) return
-      this.command += event.key
-    }
-  },
-  watch: {
-    currentPage (value) {
-      this.goToPage(value)
-    }
-  },
-  mounted () {
-    window.addEventListener('keydown', this.cmdListener)
-  },
-  destroyed () {
-    window.removeEventListener('keydown', this.cmdListener)
-  }
+  components: { TitleScreen, TextScreen, BackButton }
 }
 </script>
--- reanimar/src/components/Home.vue
+++ reanimar/src/components/Home.vue
@@ -4,7 +4,6 @@
     :bg='require("../../static/title.jpg")'
     :title='text.appTitle'
     :caption='text.appCaption'
-    ref='page1'
   )
 
   title-screen(
@@ -12,14 +11,11 @@
     :caption='text.actOne.caption'
     :bg='require("../../static/act_i.jpg")'
     linkTo='/problema'
-    ref='page2'
+
   )
 
-  title-screen(:title='text.actTwo.title' ref='page3')
-  title-screen(:title='text.actThree.title' ref='page4')
-
-  div(v-if='command' style='position: fixed; top: 5vh; left: 0; width: 100%')
-    input(style='display: block; margin: 0 auto; width: 15em' v-model='command')
+  title-screen(:title='text.actTwo.title' linkTo='/causa')
+  title-screen(:title='text.actThree.title')
 </template>
 
 <script>
@@ -30,45 +26,6 @@ import TextManager from './TextManager'
 export default {
   name: 'Home',
   mixins: [TextManager],
-  components: { TitleScreen, TextScreen },
-  data () {
-    return {
-      currentPage: 1,
-      command: ''
-    }
-  },
-  methods: {
-    goToPage (num) {
-      window.scrollTo({
-        top: this.$refs[`page${num}`].$el.offsetTop,
-        left: 0,
-        behavior: 'smooth'
-      })
-    },
-    processCommand (cmd) {
-      if (cmd === 'prox') this.currentPage += 1
-      if (cmd === 'ant') this.currentPage -= 1
-    },
-    cmdListener (event) {
-      if (event.key === 'Enter') {
-        const currentCommand = this.command
-        this.processCommand(currentCommand)
-        this.command = ''
-      }
-      if (!event.key.match(/^\w$/)) return
-      this.command += event.key
-    }
-  },
-  watch: {
-    currentPage (value) {
-      this.goToPage(value)
-    }
-  },
-  mounted () {
-    window.addEventListener('keydown', this.cmdListener)
-  },
-  destroyed () {
-    window.removeEventListener('keydown', this.cmdListener)
-  }
+  components: { TitleScreen, TextScreen }
 }
 </script>
