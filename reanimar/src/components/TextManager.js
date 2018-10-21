0bc5112033654a365f641fdd21b52a7130b5975b
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-10-20 22:38:53 -0300

    change url for content in production

--- reanimar/src/components/TextManager.js
+++ reanimar/src/components/TextManager.js
@@ -3,7 +3,7 @@ import YAML from 'yaml'
 import content from '../content.yaml'
 
 const isProd = process.env.NODE_ENV !== 'development'
-const RawYamlUrl = 'https://pastebin.com/raw/cm2jYsdG'
+const RawYamlUrl = '/reanimar/reanimar/src/content.yaml'
 
 const textContent = (async () => {
   if (!isProd) return
